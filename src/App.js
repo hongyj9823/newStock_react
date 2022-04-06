ed98bc0b147374147f8c8927682545b49b0a7744
hongyj9823 <hongyj9823@gmail.com>
2022-04-06 22:13:54 +0900

    ADD chart file & Treemapchart file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Chart from './component/Chart.js'
+import TreemapChart from './component/TreemapChart.js'
 
 function App() {
   return (
     <div className="App">
       <Chart/>
+      <TreemapChart/>
     </div>
   );
 }
